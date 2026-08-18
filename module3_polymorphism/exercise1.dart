8d94052df0b85fa4d61f2f853d083489dec7d685
safwan@zetvest.com <safwan@zetvest.com>
2026-08-18 10:10:55 +0800

    Polymorpishm excercise 1 completed

--- module3_polymorphism/exercise1.dart
+++ module3_polymorphism/exercise1.dart
@@ -29,34 +29,72 @@
 //    - Loop through and call displayArea() for each
 //    - Show that same method call works for all!
 
-// STARTER CODE:
-// class Shape {
-//   // TODO: add methods
-// }
-//
-// class Circle extends Shape {
-//   // TODO: add fields, constructor, override area()
-// }
-//
-// class Rectangle extends Shape {
-//   // TODO: add fields, constructor, override area()
-// }
-//
-// class Triangle extends Shape {
-//   // TODO: add fields, constructor, override area()
-// }
-//
-// void main() {
-//   List<Shape> shapes = [
-//     Circle(5),
-//     Rectangle(4, 6),
-//     Triangle(3, 4),
-//   ];
-//
-//   for (var shape in shapes) {
-//     shape.displayArea();
-//   }
-// }
+//STARTER CODE:
+class Shape {
+  // TODO: add methods
+  double area(){
+    print("Calculating area...");
+    return 0.0;
+  }
+
+  double displayArea(){
+    print("Area: ${area()}");
+    return area();
+  }
+
+}
+
+class Circle extends Shape {
+  // TODO: add fields, constructor, override area()
+  double radius;
+  Circle(this.radius);
+  @override
+  double area(){
+    print("Calculating area...");
+    return 3.14159 * radius * radius;
+  }
+
+}
+
+class Rectangle extends Shape {
+  // TODO: add fields, constructor, override area()
+  double width;
+  double height;
+
+    Rectangle(this.width, this.height);
+
+    @override
+    double area(){
+        print("Calculating area...");
+        return width * height;
+        }
+}
+
+class Triangle extends Shape {
+  // TODO: add fields, constructor, override area()
+  double base;
+  double height;
+
+  Triangle(this.base, this.height);
+
+  @override
+  double area(){
+    print("Calculating area...");
+    return (base * height) / 2;
+  }
+}
+
+void main() {
+  List<Shape> shapes = [
+    Circle(5),
+    Rectangle(4, 6),
+    Triangle(3, 4),
+  ];
+
+  for (var shape in shapes) {
+    shape.displayArea();
+  }
+}
 
 // EXPECTED OUTPUT (approximately):
 // Calculating area...
